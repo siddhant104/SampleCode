0816b03c2b284fb1758d5cdb50cc1acbd9a38cd5
Siddhant <Siddhant>
2016-04-24 23:56:25 +0530

    change Login.tes

--- src/main/java/com/qtpselenium/app/salesforce/login/LoginTest.java
+++ src/main/java/com/qtpselenium/app/salesforce/login/LoginTest.java
@@ -12,7 +12,7 @@ import cucumber.api.java.en.Then;
 
 //step layer
 public class LoginTest {
-	WebConnector selenium = WebConnector.getInstance(); //Siddhant Jain
+	WebConnector selenium = WebConnector.getInstance(); //Siddhant JaiShsishkdjasldjasjdn
 	
 	@Given("^I go to \"([^\"]*)\" on \"([^\"]*)\"$")
 	public void I_Go_To_Salesforce(String URL,String browser){
